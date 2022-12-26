375b01994d31660273e4b593fd96fa73a63f97fe
Earendil <marinelli.matteo.rhcp@gmail.com>
2022-12-26 16:46:48 +0100

    uhd: changed display health and mana

--- src/render/uhd/uhd.c
+++ src/render/uhd/uhd.c
@@ -6,7 +6,7 @@
 /*   By: earendil <earendil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 23:47:59 by earendil          #+#    #+#             */
-/*   Updated: 2022/12/24 18:48:59 by earendil         ###   ########.fr       */
+/*   Updated: 2022/12/26 16:40:50 by earendil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ void	render_health_bar(t_game *g)
 	const size_t		full = (
 		(g->screen_handle.width * 80.0f) / 100
 		);
-	const size_t		horizontal_offset = (
-		(g->screen_handle.width * 10.0f) / 100
-		);
 	const size_t		health = (full * g->player.hp) / 100.0f;
+	const size_t		horizontal_offset = (
+		g->screen_handle.width / 2 - health / 2
+	);
 	const t_color		health_bar_color = (t_color){255, 0, 0, 1};
 
 	render_bar(
@@ -43,10 +43,10 @@ void	render_mana_bar(t_game *g)
 	const size_t		full = (
 		(g->screen_handle.width * 50.0f) / 100.0f
 		);
-	const size_t		horizontal_offset = (
-		(g->screen_handle.width * 25.0f) / 100
-		);
 	const size_t		mana = (full * g->player.mana) / 100.0f;
+	const size_t		horizontal_offset = (
+		g->screen_handle.width / 2 - mana / 2
+	);
 	const t_color		mana_bar_color = (t_color){0, 0, 255, 1};
 
 	render_bar(
@@ -94,21 +94,21 @@ static void	render_bar(
 {
 	const size_t	width = dimensions->x;
 	const size_t	height = dimensions->y;
-	t_2d_point	px;
+	t_2d_point		px;
 
 	if (0 == width || 0 == height)
 		return ;
 	px.y = 0;
 	while (px.y < (int)height)
 	{
-		px.x = 0;
-		while (px.x < (int)width)
+		px.x = offset->x;
+		while (px.x < offset->x + (int)width)
 		{
 			ft_put_px_to_image(
 				&g->screen_handle.frame_data,
 				ft_get_pixel_offset(
 					&g->screen_handle.frame_data,
-					(t_int_2d_point){px.x + offset->x, px.y + offset->y}
+					(t_int_2d_point){px.x, px.y + offset->y}
 					),
 				*bar_color
 			);
