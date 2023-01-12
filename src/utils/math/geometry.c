97e586e3e2c62f140305a7ac0f5fc58b8fae7a37
Alberto Lazzaris <alb.lazzaris@gmail.com>
2023-01-12 13:42:33 +0100

    libft/src && libft/math (not at all) Normed

--- src/utils/math/geometry.c
+++ src/utils/math/geometry.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   geometry.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: earendil <earendil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alazzari <alazzari@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 11:50:00 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/12/23 21:40:35 by earendil         ###   ########.fr       */
+/*   Updated: 2023/01/12 13:41:13 by alazzari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 t_2d_point	ft_rotate(t_2d_point vector, float ray_angle)
 {
-	const t_2d_matrix	rotation_matrix = ft_get_new_2dmtrx(
+	const t_2d_matrix	rotation_matrix = ft_get_new_2dmtrx (
 			ft_get_new_2dpt(cos(ray_angle), -sin(ray_angle)),
 			ft_get_new_2dpt(sin(ray_angle), cos(ray_angle))
-		);
+			);
+
 	return (ft_matrix_multiplication(rotation_matrix, vector));
 }
